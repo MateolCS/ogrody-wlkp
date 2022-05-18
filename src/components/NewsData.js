bf2ee0bc81b472c2115e5d46cbf1056dd2398443
MateoLCS <mateusz.namysl0318@gmail.com>
2022-05-18 15:11:48 +0200

    Created newsData

---
+++ src/components/NewsData.js
@@ -0,0 +1,238 @@
+import firmahl1 from '././../assets/firmaHL/firmahl1.jpg';
+import firmahl2 from '././../assets/firmaHL/firmahl2.jpg';
+import firmahl3 from '././../assets/firmaHL/firmahl3.jpg';
+import firmahl4 from '././../assets/firmaHL/firmahl4.jpg';
+import firmahl5 from '././../assets/firmaHL/firmahl5.jpg';
+
+
+
+
+
+
+export const NewsData =[
+    {
+        id: 0, 
+        title: 'Trawnik w jeden dzień', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '13 marca 2019', 
+        category: 'news',
+        investor: '',
+        content: 'Czy można założyć trawnik w jeden dzień? Udowadniamy że jest to możliwe.',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 1, 
+        title: 'Firma HL', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '26 lutego 2019', 
+        category: 'ogrody',
+        investor: 'Firma HL',
+        content: '',
+        imgs: [
+            {id: 0, url: firmahl1},
+            {id: 1, url: firmahl2},
+            {id: 2, url: firmahl3},
+            {id: 3, url: firmahl4},
+            {id: 4, url: firmahl5},
+        ],
+    },
+    {
+        id: 2, 
+        title: 'Gmina Wągrowiec', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '26 lutego 2019', 
+        category: 'ogrody',
+        investor: 'Gmina Wągrowiec',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 3, 
+        title: 'Gmina Szamocin', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '26 lutego 2019', 
+        category: 'Ogrody',
+        investor: 'Gmina Szamocin',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 4, 
+        title: 'Gmina Budzyń', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '25 lutego 2019', 
+        category: 'news',
+        investor: 'Gmina Budzyń',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 5, 
+        title: 'Margonim', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '13 marca 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: 'Lorem ipsum dolor sit amet, mea cu omnium urbanitas, labitur volumus id eum. Ius ignota offendit similique et, sea dolorum vituperata ullamcorper et, justo insolens omittantur sit ne. Aliquip pertinax vix ad, ea eos euismod officiis. Utamur minimum repudiare qui ex. Rationibus theophrastus his ut, eum iudico pericula no. Mei id fugit dolor. Has ne legere tibique. At eam decore probatus. Delenit interesset an mei, ut eos tota vivendo deseruisse. Est at facete delenit democritum. Te nihil aliquip ornatus cum.',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 6, 
+        title: 'Wągrowiec', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '27 października 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 7, 
+        title: 'Klub Sportowy Sokół', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '27 października 2018', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 8, 
+        title: 'Chodzież2', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '27 października 2018', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 9, 
+        title: 'Realizacja1', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '14 lutego 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 10, 
+        title: 'Realizacja2', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '14 lutego 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 11, 
+        title: 'Realizacja3', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '14 lutego 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 12, 
+        title: 'Realizacja4', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '14 lutego 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 13, 
+        title: 'Realizacja5', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '14 lutego 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 14, 
+        title: 'Realizacja6', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '14 lutego 2019', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 15, 
+        title: 'Chodzież1', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '27 października 2018', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+    {
+        id: 16, 
+        title: 'Rataje', 
+        imgUrl: '', 
+        author: 'juzer', 
+        date: '27 października 2014', 
+        category: 'Ogrody',
+        investor: '',
+        content: '',
+        imgs: [
+
+        ],
+    },
+]
